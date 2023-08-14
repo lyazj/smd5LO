38911ef7655c2ee7229e4172f97c63d5508da370
lyazj <seeson@pku.edu.cn>
2023-08-14 22:53:42 +0800

    Revise plot and directory structure.

--- src/plot.cpp
+++ src/plot.cpp
@@ -1,7 +1,23 @@
-#include "TFile.h"
-#include "TTree.h"
+#include "smd5/utils.h"
+#include <iostream>
+#include <errno.h>
 
-int main()
+using namespace std;
+
+int main(int argc, char *argv[])
 {
-  return 0;  // TODO
+  if(argc != 2) {
+    cerr << "usage: " << program_invocation_short_name << " <proc-dir>" << endl;
+    return 1;
+  }
+  string basedir = argv[1];
+  if(basedir.empty()) basedir = ".";
+  if(basedir.back() != '/') basedir.push_back('/');
+
+  for(const Mg5Run &mg5run : list_run(basedir)) {
+    string lhepath = basedir + mg5run.path + "/Events/run_01/unweighted_events.root";
+    string cmd = "ls " + lhepath;
+    system(cmd.c_str());
+  }
+  return 0;
 }
