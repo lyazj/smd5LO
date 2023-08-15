eeca581a6fa799da0bc3ee6868478881aa6f0433
lyazj <seeson@pku.edu.cn>
2023-08-15 22:36:07 +0800

    Update plot program.

--- src/plot.cpp
+++ src/plot.cpp
@@ -46,6 +46,9 @@ static TH1F *format(TH1F *th1f)
   th1f->SetBit(th1f->kNoTitle | th1f->kNoStats);
   th1f->GetXaxis()->SetTitleOffset(1.2);
   th1f->GetYaxis()->SetTitleOffset(1.3);
+  string title = th1f->GetTitle();
+  title += " (nevt=" + to_string((Long64_t)th1f->GetEntries()) + ")";
+  th1f->SetTitle(title.c_str());
   return th1f;
 }
 
@@ -62,16 +65,33 @@ void plot(string basedir)
 
   Int_t nbin = 30;
   Float_t pt_min = 0.0, pt_max = 1500.0;
+  Float_t eta_min = -3.0, eta_max = 3.0;
   Float_t m_min = 0.0, m_max = 1500.0;
   const char *label[2] = {
     "\\font[132]{pp \\rightarrow H\\mu^{\\pm}\\mu^{\\pm}jj}",
     "\\font[132]{pp \\rightarrow HH\\mu^{\\pm}\\mu^{\\pm}jj}",
   };
-  shared_ptr<TH1F> pt_max_mu[2] = {
+  shared_ptr<TH1F> pt_mu[2] = {
     make_shared<TH1F>("", label[0], nbin, pt_min, pt_max),
     make_shared<TH1F>("", label[1], nbin, pt_min, pt_max),
   };
-  shared_ptr<TH1F> m_inv_mu[2] = {
+  shared_ptr<TH1F> eta_mu[2] = {
+    make_shared<TH1F>("", label[0], nbin, eta_min, eta_max),
+    make_shared<TH1F>("", label[1], nbin, eta_min, eta_max),
+  };
+  shared_ptr<TH1F> m_mu[2] = {
+    make_shared<TH1F>("", label[0], nbin, m_min, m_max),
+    make_shared<TH1F>("", label[1], nbin, m_min, m_max),
+  };
+  shared_ptr<TH1F> pt_h[2] = {
+    make_shared<TH1F>("", label[0], nbin, pt_min, pt_max),
+    make_shared<TH1F>("", label[1], nbin, pt_min, pt_max),
+  };
+  shared_ptr<TH1F> eta_h[2] = {
+    make_shared<TH1F>("", label[0], nbin, eta_min, eta_max),
+    make_shared<TH1F>("", label[1], nbin, eta_min, eta_max),
+  };
+  shared_ptr<TH1F> m_h[2] = {
     make_shared<TH1F>("", label[0], nbin, m_min, m_max),
     make_shared<TH1F>("", label[1], nbin, m_min, m_max),
   };
@@ -131,8 +151,12 @@ void plot(string basedir)
         cerr << "ERROR: unexpected nh=" << nh << " and nmu=" << nmu << endl;
         continue;
       }
-      pt_max_mu[nh - 1]->Fill(max(pmu[0].Pt(), pmu[1].Pt()));
-      m_inv_mu[nh - 1]->Fill((pmu[0] + pmu[1]).M());
+      pt_mu[nh - 1]->Fill((pmu[0] + pmu[1]).Pt());
+      eta_mu[nh - 1]->Fill((pmu[0] + pmu[1]).Eta());
+      m_mu[nh - 1]->Fill((pmu[0] + pmu[1]).M());
+      pt_h[nh - 1]->Fill((ph[0] + ph[1]).Pt());
+      eta_h[nh - 1]->Fill((ph[0] + ph[1]).Eta());
+      m_h[nh - 1]->Fill((ph[0] + ph[1]).M());
 
       if((i + 1) % 1000 == 0) {
         clog << "INFO: " << setw(6) << (i + 1) << " events processed" << endl;
@@ -148,22 +172,62 @@ void plot(string basedir)
   TLegend *legend;
 
   for(int i = 0; i < 2; ++i) {
-    pt_max_mu[i]->SetXTitle("p_{T}^{\\mu,max}");
-    pt_max_mu[i]->SetYTitle("density");
-    pt_max_mu[i]->SetLineColor(i + 2);
-    format(pt_max_mu[i].get())->DrawNormalized(i == 0 ? "" : "SAME");
+    pt_mu[i]->SetXTitle("p_{T}^{\\mu}");
+    pt_mu[i]->SetYTitle("density");
+    pt_mu[i]->SetLineColor(i + 2);
+    format(pt_mu[i].get())->DrawNormalized(i == 0 ? "" : "SAME");
   }
-  legend = canvas->BuildLegend(0.7, 0.9, 0.9, 0.8);
+  legend = canvas->BuildLegend(0.6, 0.9, 0.95, 0.8);
   legend->SetTextSize(0.03);
   canvas->SaveAs("pt_max_mu.pdf");
 
   for(int i = 0; i < 2; ++i) {
-    m_inv_mu[i]->SetXTitle("m_{inv}^{\\mu^{+}\\mu^{-}}");
-    m_inv_mu[i]->SetYTitle("density");
-    m_inv_mu[i]->SetLineColor(i + 2);
-    format(m_inv_mu[i].get())->DrawNormalized(i == 0 ? "" : "SAME");
+    eta_mu[i]->SetXTitle("\\eta^{\\mu}");
+    eta_mu[i]->SetYTitle("density");
+    eta_mu[i]->SetLineColor(i + 2);
+    format(eta_mu[i].get())->DrawNormalized(i == 0 ? "" : "SAME");
   }
-  legend = canvas->BuildLegend(0.7, 0.9, 0.9, 0.8);
+  legend = canvas->BuildLegend(0.6, 0.9, 0.95, 0.8);
   legend->SetTextSize(0.03);
-  canvas->SaveAs("m_inv_mu.pdf");
+  canvas->SaveAs("eta_mu.pdf");
+
+  for(int i = 0; i < 2; ++i) {
+    m_mu[i]->SetXTitle("m_{inv}^{\\mu}");
+    m_mu[i]->SetYTitle("density");
+    m_mu[i]->SetLineColor(i + 2);
+    format(m_mu[i].get())->DrawNormalized(i == 0 ? "" : "SAME");
+  }
+  legend = canvas->BuildLegend(0.6, 0.9, 0.95, 0.8);
+  legend->SetTextSize(0.03);
+  canvas->SaveAs("m_mu.pdf");
+
+  for(int i = 0; i < 2; ++i) {
+    pt_h[i]->SetXTitle("p_{T}^{H}");
+    pt_h[i]->SetYTitle("density");
+    pt_h[i]->SetLineColor(i + 2);
+    format(pt_h[i].get())->DrawNormalized(i == 0 ? "" : "SAME");
+  }
+  legend = canvas->BuildLegend(0.6, 0.9, 0.95, 0.8);
+  legend->SetTextSize(0.03);
+  canvas->SaveAs("pt_h.pdf");
+
+  for(int i = 0; i < 2; ++i) {
+    eta_h[i]->SetXTitle("\\eta^{H}");
+    eta_h[i]->SetYTitle("density");
+    eta_h[i]->SetLineColor(i + 2);
+    format(eta_h[i].get())->DrawNormalized(i == 0 ? "" : "SAME");
+  }
+  legend = canvas->BuildLegend(0.6, 0.9, 0.95, 0.8);
+  legend->SetTextSize(0.03);
+  canvas->SaveAs("eta_h.pdf");
+
+  for(int i = 0; i < 2; ++i) {
+    m_h[i]->SetXTitle("m_{inv}^{H}");
+    m_h[i]->SetYTitle("density");
+    m_h[i]->SetLineColor(i + 2);
+    format(m_h[i].get())->DrawNormalized(i == 0 ? "" : "SAME");
+  }
+  legend = canvas->BuildLegend(0.6, 0.9, 0.95, 0.8);
+  legend->SetTextSize(0.03);
+  canvas->SaveAs("m_h.pdf");
 }
